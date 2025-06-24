74fc439ba705c36dece8ca7c684abfb7d49bf64b
Ronnie <pastorronnie@mac.com>
2025-06-23 20:32:20 -0500

    Update TailwindCSS

--- client-vite/postcss.config.js
+++ client-vite/postcss.config.js
@@ -1,8 +1,6 @@
-// postcss.config.js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 
+module.exports = {
+  plugins: [require('@tailwindcss/postcss')(), require('autoprefixer')],
+};
